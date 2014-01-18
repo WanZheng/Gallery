9719179d850b0d36726374829e00eeefb6293acb
Wilson Wan <wanzheng@gmail.com>
2014-01-18 21:33:30 +0800

    基本布局

--- src/me/wanzheng/gallery/Gallery.java
+++ src/me/wanzheng/gallery/Gallery.java
@@ -2,14 +2,25 @@ package me.wanzheng.gallery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class Gallery extends Activity
 {
-    /** Called when the activity is first created. */
+    private ArrayAdapter<String> adapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        GridView gridView = (GridView)findViewById(R.id.gridView);
+        adapter = new ArrayAdapter<String>(this, R.layout.text_node, R.id.text);
+        gridView.setAdapter(adapter);
+
+        for (int i=0; i<10; i++) {
+            adapter.add("123");
+        }
     }
 }
