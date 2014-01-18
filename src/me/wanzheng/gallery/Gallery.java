0d1f1edb598dc89c677276c0b6bf1d7b743cbe44
Wilson Wan <wanzheng@gmail.com>
2014-01-18 20:56:52 +0800

    create android project

---
+++ src/me/wanzheng/gallery/Gallery.java
@@ -0,0 +1,15 @@
+package me.wanzheng.gallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gallery extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
